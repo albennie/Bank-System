b83ed5b13f61639ae6c54ba0891f2d75087b4941
albennie <benjamin.sh2006@gmail.com>
2025-07-21 00:25:39 +0330

    Complete The Login Form(Logic)

--- BankSystem/DASHBOARDS/LoginForm.cpp
+++ BankSystem/DASHBOARDS/LoginForm.cpp
@@ -1,14 +1,58 @@
 #include "LoginForm.h"
 #include "ui_LoginForm.h"
+#include "CustomerDashboard.h"
 
-LoginForm::LoginForm(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::LoginForm)
+LoginForm::LoginForm(std::shared_ptr<BankSystem> system,QWidget* parent)
+    : QMainWindow(parent), ui(new Ui::LoginForm)
 {
-    ui->setupUi(this);
+        bankSystem = system;
+        ui->setupUi(this);
 }
 
 LoginForm::~LoginForm()
 {
     delete ui;
 }
+
+void LoginForm::on_loginButton_clicked()
+{
+    QString username = ui->user->text();
+    QString password = ui->pass->text();
+
+    if (bankSystem->login(username.toStdString(), password.toStdString())) {
+        this->hide();
+
+        if (bankSystem->isAdminLoggedIn())
+        {
+            adminUI = new AdminDashboard(bankSystem, this);
+            connect(adminUI, &AdminDashboard::logoutRequested, this, &LoginForm::showAgain);
+            adminUI->show();
+        } else {
+            customerUI = new CustomerDashboard(bankSystem, this);
+            connect(customerUI, &CustomerDashboard::logoutRequested, this, &LoginForm::showAgain);
+            customerUI->show();
+        }
+    } else {
+        ui->errorLabel->setText("Wrong Data!");
+    }
+}
+
+void LoginForm::showAgain() {
+    if (customerUI) {
+        customerUI->close();
+        delete customerUI;
+        customerUI = nullptr;
+    }
+    if (adminUI) {
+        adminUI->close();
+        delete adminUI;
+        adminUI = nullptr;
+    }
+    bankSystem->logout();
+    this->ui->pass->setText("");
+    this->ui->user->setText("");
+    ui->errorLabel->setText("");
+    this->show();
+}
+
+
